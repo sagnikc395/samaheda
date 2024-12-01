8b97b9ca48aa503634ad5d2929476c7e8b00a571
sagnikc395 <sagnikchatterjee395@gmail.com>
2024-12-01 23:38:39 +0530

    added changes

--- cmd/myshell/main.go
+++ cmd/myshell/main.go
@@ -5,11 +5,42 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"os/exec"
+	"path/filepath"
 	"strings"
 )
 
 // Ensures gofmt doesn't remove the "fmt" import in stage 1 (feel free to remove this!)
 var _ = fmt.Fprint
+
+//check if the program present in PATH
+//https://xnacly.me/posts/2023/go-check-for-executable/#:~:text=To%20check%20if%20an%20executable,its%20exported%20LookPath()%20function.
+
+func checkPath(program string) (string, error) {
+	pathenv := os.Getenv("PATH")
+	paths := strings.Split(pathenv, string(os.PathListSeparator))
+
+	_ = paths
+	execpath, err := exec.LookPath(program)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	absPath, err := filepath.Abs(execpath)
+	if err != nil {
+		return program, err
+	}
+
+	// for _, path := range paths {
+	// 	if _, err := os.Stat(path); err != nil {
+	// 		fmt.Printf("- %s (not accessible)\n", path)
+	// 	} else {
+	// 		fmt.Printf("- %s\n", path)
+	// 	}
+	// }
+
+	return absPath, nil
+}
 
 func main() {
 	// Uncomment this block to pass the first stage
@@ -30,15 +61,11 @@ func main() {
 		} else if cmds[0] == "exit" {
 			os.Exit(0)
 		} else if cmds[0] == "type" {
-			if cmds[1] == "echo" {
-				fmt.Println("echo is a shell builtin")
-			} else if cmds[1] == "exit" {
-				fmt.Println("exit is a shell builtin")
-			} else if cmds[1] == "type" {
-				fmt.Println("type is a shell builtin")
-			} else {
-				fmt.Printf("%s: not found\n", cmds[1])
+			result, err := checkPath(cmds[1])
+			if err != nil {
+				fmt.Printf("%s: not found\n", result)
 			}
+			fmt.Printf("%s is %s\n", cmds[1], result)
 		} else {
 			fmt.Printf("%s: command not found\n", cmd)
 		}

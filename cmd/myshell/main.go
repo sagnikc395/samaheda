f71cb9e871719ca123fefe6e0903c98abbe02206
sagnikc395 <sagnikchatterjee395@gmail.com>
2024-12-08 23:44:20 +0530

    src: added relative paths

--- cmd/myshell/main.go
+++ cmd/myshell/main.go
@@ -5,69 +5,8 @@ import (
 	"fmt"
 	"log"
 	"os"
-	"os/exec"
-	"path/filepath"
 	"strings"
 )
-
-var builtins = map[string]bool{
-	"echo": true,
-	"exit": true,
-	"type": true,
-	"pwd":  true,
-}
-
-func checkPath(program string) (string, error) {
-	pathenv := os.Getenv("PATH")
-	paths := strings.Split(pathenv, string(os.PathListSeparator))
-
-	_ = paths
-	execpath, err := exec.LookPath(program)
-	if err != nil {
-		return program, err
-	}
-
-	absPath, err := filepath.Abs(execpath)
-	if err != nil {
-		return program, err
-	}
-
-	return absPath, nil
-}
-
-func execCommand(program string) (string, error) {
-	commands := strings.Split(program, " ")
-	cmdExists, err := checkPath(commands[0])
-	if err != nil {
-		return "", err
-	}
-
-	cmd := exec.Command(cmdExists, commands[1:]...)
-	out, err := cmd.CombinedOutput()
-	if err != nil {
-		return "", err
-	}
-	return strings.TrimRight(string(out), "\r\n"), nil
-}
-
-func printCurrentWorkingDir() string {
-	dir, err := os.Getwd()
-	_ = err
-	return dir
-}
-
-func changeDirIfExists(path string) bool {
-	if _, err := os.Stat(path); os.IsNotExist(err) {
-		return false
-	}
-
-	err := os.Chdir(path)
-	if err != nil {
-		return false
-	}
-
-	return true
-}
 
 func main() {
 	reader := bufio.NewReader(os.Stdin)
